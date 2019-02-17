387a02ae5b4ed80d89dea9bf5e3f5157aae8d610
midhat5mm <m.manxholli@gmail.com>
2019-02-17 17:27:55 +0100

    jwt token

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const products = require('./routes/api/products');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 const db = require('./config/keys').mongoURI;
 
@@ -12,7 +17,9 @@ mongoose.connect(db, { useNewUrlParser: true })
         .then(() => console.log('Connected'))
         .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello wwww'));
+app.use(passport.initialize());
+
+require('./config/passport')(passport);
 
 app.use('/api/users', users);
 app.use('/api/products', products);
